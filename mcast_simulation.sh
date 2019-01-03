eb38ba6e6b20abe0dcdcfb3a3ef1acfa1a0a2365
sslaxman <45185945+sslaxman@users.noreply.github.com>
2019-01-03 09:49:29 -0800

    Create mcast_simulation.sh

---
+++ mcast_simulation.sh
@@ -0,0 +1,9 @@
+[root@B1-Z2-lnx1 lakshman-tools]# more tsplay-harmob.sh 
+./tsplay -loop -i 200.2.0.100 /var/www/html/v2pc/ATT/har-mobile/CH1-p1.ts 232.5.5.1:50001 &
+./tsplay -loop -i 200.2.0.100 /var/www/html/v2pc/ATT/har-mobile/CH1-p2.ts 232.5.5.1:50002 &
+./tsplay -loop -i 200.2.0.100 /var/www/html/v2pc/ATT/har-mobile/CH1-p3.ts 232.5.5.1:50003 &
+./tsplay -loop -i 200.2.0.100 /var/www/html/v2pc/ATT/har-mobile/CH1-p4.ts 232.5.5.1:50004 &
+./tsplay -loop -i 200.2.0.100 /var/www/html/v2pc/ATT/har-mobile/CH1-p5.ts 232.5.5.1:50005 &
+./tsplay -loop -i 200.2.0.100 /var/www/html/v2pc/ATT/har-mobile/CH1-p6.ts 232.5.5.1:50006 &
+./tsplay -loop -i 200.2.0.100 /var/www/html/v2pc/ATT/har-mobile/CH1-p7.ts 232.5.5.1:50007 &
+[root@B1-Z2-lnx1 lakshman-tools]# 
